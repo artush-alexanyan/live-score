059f7459e61132c218c8da6f758190b4744146fc
Norayr1999 <tamazyan.norayr20@gmail.com>
2023-01-25 16:25:17 +0400

    adding homepage hero

--- src/components/scores/LiveScores.vue
+++ src/components/scores/LiveScores.vue
@@ -1,21 +1,18 @@
 <template>
-    <div class="live-scores bg-slate-800">
-        <h1 class="text-center flex items-center justify-center text-white text-2xl pt-5">
+    <div class="live-scores bg--800">
+        <h1 class="text-center flex items-center justify-center text-2xl pt-5">
             Live Scores
             <span class="h-4 w-4 bg-red-600 rounded-full ml-3"></span>
         </h1>
         <div class="mx-auto p-5 md:w-3/4" v-if="!loading">
             <ul class="py-10">
                 <li 
-                    class="flex items-center justify-between py-2 px-3 bg-white my-[0.125rem] rounded shadow" 
+                    class="flex items-center justify-between py-6 px-3 bg-white my-[0.125rem] border-b border-b-gray-300" 
                     v-for="(match, index) in liveScores" 
                     :key="index"
                     @click="viewMatch(match, index)"
                 >
                     <div class="flex items-center">
-                        <div> 
-                            ⭐
-                        </div>
                         <div class="minutes ml-5">
                             <span class="text-red-600 font-bold">
                                 {{  match.time  }}
@@ -33,7 +30,7 @@
                         </div>
                         <router-link 
                             :to="`/match-deatails/${ match.away_name + '_' + match.home_name }`" 
-                            class="text-xs text-blue-600 ml-3"
+                            class="md:text-lg text-xs text-white md:p-3 p-2 ml-8 bg-blue-600"
                         >
                             View
                         </router-link>
@@ -41,7 +38,7 @@
                 </li>
             </ul>            
         </div>
-        <div class="flex items-center justify-center text-4xl text-white w-full h-screen" v-else>
+        <div class="flex items-center justify-center text-4xl  w-full h-screen" v-else>
             Wait⚽⚽⚽
         </div>
     </div>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="app-home h-screen">
-        <h1 class="text-center text-5xl py-10">App Home</h1>
+        <div class="relative bg-[url('../assets/seria-a.jpg')] h-full bg-center bg-no-repeat w-full bg-cover"></div>  
+        <div class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 h-1/2 md:w-1/2 w-full text-white p-8">
+            <h1 class="text-center text-3xl">
+                Welcome to Seria A
+            </h1>
+            <p class="text-center py-5 md:text-lg text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore accusamus, delectus saepe reiciendis enim,  blanditiis illum beatae!</p>
+            <div class="py-10 text-center">
+                <router-link class="md:px-10 px-2 md:py-4 py-2 bg-blue-700 hover:bg-blue-900" to="/live">
+                    Explore games
+                </router-link>
+            </div>
+        </div>
     </div>
 </template>
 
