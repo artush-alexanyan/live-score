19f470d8aad9433ff4f90c7b9d95e7223a1003db
Norayr1999 <tamazyan.norayr20@gmail.com>
2023-01-25 17:00:39 +0400

    adding loaders

--- src/components/scores/MatchDetails.vue
+++ src/components/scores/MatchDetails.vue
@@ -1,142 +1,146 @@
 <template>
     <div class="match-details h-screen">
-
-        <div class="bg-gray-100 flex items-center justify-center">
-            <h1 class="text-2xl py-3">Match details </h1>
-        </div>
-
-        <div class="flex items-center justify-between p-5 mx-auto md:w-3/4 border-b border--gray-100">
-            <h2 class="md:text-3xl">
-                {{ currentMatch.home_name }}
-            </h2>
-            <div class="score-details grid justify-items-center">
-                <span> {{ currentMatch.rawdate }} </span>
-                <div class="match-score flex items-center md:text-[5rem] font-bold">
-                    <span class="my-2">
-                        {{ currentMatch.home_score }}
-                    </span>
-                    <span>-</span>
-                    <span class="my-2">
-                        {{ currentMatch.away_score }}
-                    </span>                    
-                </div>
-                <span class="uppercase"> {{ currentMatch.time }} </span>
+        <div v-if="!loading">
+            <div class="bg-gray-100 flex items-center justify-center">
+                <h1 class="text-2xl py-3">Match details </h1>
             </div>
-            <h2 class="md:text-3xl">
-                {{ currentMatch.away_name }}
-            </h2>            
-        </div>
-        <div class="flex items-center justify-between py-5 mx-auto md:w-3/4 border-b border-b-gray-100">
-            <ul class="flex items-center md:justify-start justify-center w-full">
-                <li 
-                    class="mx-1 rounded border border-gray-300 md:px-3 md:py-2 p-2 cursor-pointer" 
-                    :class="currentTab === index ? 'bg-slate-900 text-white' : ''"
-                    v-for="(tab, index) in tabs" 
-                    :key="index"
-                    @click="changeTab(index)"
-                >
-                    {{ tab }} 
-                </li>
-            </ul>
-        </div> 
-        <div class="lineups flex justify-between md:w-3/4 mx-auto p-5" v-show="currentTab === 1">
-            <ul>
-                <li v-for="(home_player, index) in currentMatch.home_lineups" :key="index">
-                    {{ index + 1 }})  {{ home_player }}
-                </li>
-            </ul>
-            <ul>
-                <li v-for="(away_player, index) in currentMatch.away_lineups" :key="index">
-                    {{ index + 1 }})  {{ away_player }}
-                </li>
-            </ul>            
-        </div>  
-        <div class="benchs flex justify-between md:w-3/4 mx-auto p-5" v-show="currentTab === 2">
-            <ul>
-                <li v-for="(home_player, index) in currentMatch.home_bench" :key="index">
-                    {{ index + 1 }})  {{ home_player }}
-                </li>
-            </ul>
-            <ul>
-                <li v-for="(away_player, index) in currentMatch.away_bench" :key="index">
-                    {{ index + 1 }})  {{ away_player }}
-                </li>
-            </ul>            
-        </div>  
-        <div 
-            class="overall md:w-3/4 mx-auto h-[28rem]" 
-            :class="getEventsLen ? 'overflow-scroll overflow-x-hidden' : ''" 
-            v-show="currentTab === 0"
-        >
-            <div v-if="getEventsLen">
-                <ul>
-                    <li class="p-3 border" v-for="(event, index) in currentMatch.events" :key="index">
-                        <span class="text-blue-700 text-lg">
-                            {{ event.type === 'ST' ? 'Game Started!' : '' }} 
+
+            <div class="flex items-center justify-between p-5 mx-auto md:w-3/4 border-b border--gray-100">
+                <h2 class="md:text-3xl">
+                    {{ currentMatch.home_name }}
+                </h2>
+                <div class="score-details grid justify-items-center">
+                    <span> {{ currentMatch.rawdate }} </span>
+                    <div class="match-score flex items-center md:text-[5rem] font-bold">
+                        <span class="my-2">
+                            {{ currentMatch.home_score }}
                         </span>
-                        <span class="text-red-700 text-lg">
-                            {{ event.type === 'F' ? 'Game Finished' : '' }} 
+                        <span>-</span>
+                        <span class="my-2">
+                            {{ currentMatch.away_score }}
                         </span>                    
-
-                        {{ event.type === 'G' ? '⚽' : '' }} 
-                        {{ event.type === 'PEN' ? '🥅' : '' }} 
-                        {{ event.type === 'YC' ? '🟨' : '' }}
-
-                        <small>
-                            {{ event.type === 'GG' ? `Goalkeeper: ${event.details}` : '' }}
-                        </small>
-                        
-                        {{ event.player }}
-                        <strong>
-                            {{ event.minute }}'
-                        </strong>
-                        <small>
-                            {{ event.in }} {{ event.type === 'S' ? '🔄' : '' }} {{ event.out }}
-                        </small>
+                    </div>
+                    <span class="uppercase"> {{ currentMatch.time }} </span>
+                </div>
+                <h2 class="md:text-3xl">
+                    {{ currentMatch.away_name }}
+                </h2>            
+            </div>
+            <div class="flex items-center justify-between py-5 mx-auto md:w-3/4 border-b border-b-gray-100">
+                <ul class="flex items-center md:justify-start justify-center w-full">
+                    <li 
+                        class="mx-1 rounded border border-gray-300 md:px-3 md:py-2 p-2 cursor-pointer" 
+                        :class="currentTab === index ? 'bg-slate-900 text-white' : ''"
+                        v-for="(tab, index) in tabs" 
+                        :key="index"
+                        @click="changeTab(index)"
+                    >
+                        {{ tab }} 
                     </li>
                 </ul>
-            </div>
-            <div v-else>
-                <p class="text-center text-2xl text-green-500 py-10">
-                    No evets to show! <br>
-                    The match is not started yet!
-                </p>
-            </div>
-        </div>        
-        <div class="overall md:w-3/4 mx-auto p-8 h-[28rem]" v-show="currentTab === 3">
-            <ul>
-                <li 
-                    class="flex items-center py-4" 
-                    v-for="(bookmaker, index) in getOdds" 
-                    :key="index"
-                >
-                    <img 
-                        class="h-6 rounded" 
-                        :src="bookmaker.image" 
-                        alt="bookMakerImage"
-                    >
-                    <h4 class="md:text-xl text-sm mx-4 hidden md:block">
-                        {{ bookmaker.id }}
-                    </h4>
-                    <ul class="flex">
-                        <li 
-                            class="flex" 
-                            :class="odd.name === '2' ? 'order-last': ''" 
-                            v-for="(odd, index) in bookmaker.odds" :key="index"
-                        >
-                            <span class="px-2 py-1 m-1 rounded border border-gray-200">
-                                <small class="text-gray-500 font-bold">
-                                {{ odd.type }}
-                                </small>  
-                                <span class="text-blue-500">
-                                {{ odd.value }}
-                                </span>
+            </div> 
+            <div class="lineups flex justify-between md:w-3/4 mx-auto p-5" v-show="currentTab === 1">
+                <ul>
+                    <li v-for="(home_player, index) in currentMatch.home_lineups" :key="index">
+                        {{ index + 1 }})  {{ home_player }}
+                    </li>
+                </ul>
+                <ul>
+                    <li v-for="(away_player, index) in currentMatch.away_lineups" :key="index">
+                        {{ index + 1 }})  {{ away_player }}
+                    </li>
+                </ul>            
+            </div>  
+            <div class="benchs flex justify-between md:w-3/4 mx-auto p-5" v-show="currentTab === 2">
+                <ul>
+                    <li v-for="(home_player, index) in currentMatch.home_bench" :key="index">
+                        {{ index + 1 }})  {{ home_player }}
+                    </li>
+                </ul>
+                <ul>
+                    <li v-for="(away_player, index) in currentMatch.away_bench" :key="index">
+                        {{ index + 1 }})  {{ away_player }}
+                    </li>
+                </ul>            
+            </div>  
+            <div 
+                class="overall md:w-3/4 mx-auto h-[28rem]" 
+                :class="getEventsLen ? 'overflow-scroll overflow-x-hidden' : ''" 
+                v-show="currentTab === 0"
+            >
+                <div v-if="getEventsLen">
+                    <ul>
+                        <li class="p-3 border" v-for="(event, index) in currentMatch.events" :key="index">
+                            <span class="text-blue-700 text-lg">
+                                {{ event.type === 'ST' ? 'Game Started!' : '' }} 
                             </span>
+                            <span class="text-red-700 text-lg">
+                                {{ event.type === 'F' ? 'Game Finished' : '' }} 
+                            </span>                    
+
+                            {{ event.type === 'G' ? '⚽' : '' }} 
+                            {{ event.type === 'PEN' ? '🥅' : '' }} 
+                            {{ event.type === 'YC' ? '🟨' : '' }}
+
+                            <small>
+                                {{ event.type === 'GG' ? `Goalkeeper: ${event.details}` : '' }}
+                            </small>
+                            
+                            {{ event.player }}
+                            <strong>
+                                {{ event.minute }}'
+                            </strong>
+                            <small>
+                                {{ event.in }} {{ event.type === 'S' ? '🔄' : '' }} {{ event.out }}
+                            </small>
                         </li>
                     </ul>
-                </li>
-            </ul>
-        </div>                       
+                </div>
+                <div v-else>
+                    <p class="text-center text-2xl text-green-500 py-10">
+                        No evets to show! <br>
+                        The match is not started yet!
+                    </p>
+                </div>
+            </div>        
+            <div class="overall md:w-3/4 mx-auto p-8 h-[28rem]" v-show="currentTab === 3">
+                <ul>
+                    <li 
+                        class="flex items-center py-4" 
+                        v-for="(bookmaker, index) in getOdds" 
+                        :key="index"
+                    >
+                        <img 
+                            class="h-6 rounded" 
+                            :src="bookmaker.image" 
+                            alt="bookMakerImage"
+                        >
+                        <h4 class="md:text-xl text-sm mx-4 hidden md:block">
+                            {{ bookmaker.id }}
+                        </h4>
+                        <ul class="flex">
+                            <li 
+                                class="flex" 
+                                :class="odd.name === '2' ? 'order-last': ''" 
+                                v-for="(odd, index) in bookmaker.odds" :key="index"
+                            >
+                                <span class="px-2 py-1 m-1 rounded border border-gray-200">
+                                    <small class="text-gray-500 font-bold">
+                                    {{ odd.type }}
+                                    </small>  
+                                    <span class="text-blue-500">
+                                    {{ odd.value }}
+                                    </span>
+                                </span>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>                       
+        </div>
+        <div class="flex items-center justify-center text-4xl  w-full h-screen" v-else>
+            Wait⚽⚽⚽
+        </div>        
     </div>
 </template>
 
@@ -149,7 +153,8 @@ export default {
     data: () => ({
         currentMatch: [],
         tabs: ['Overall','Lineups', 'Benchs', 'Odds'],
-        currentTab: 0
+        currentTab: 0,
+        loading: false
     }),
     computed: {
         getOdds () {
@@ -161,11 +166,13 @@ export default {
     },
     methods: {
         getCurrentMatch () {
+            this.loading = true
             BASE_URL.get('livescores/').then(res => {
                 let scores = res.data.scores
                 scores.map(match => {
                     if(this.$route.params.id === match.away_name + '_' + match.home_name){
                         this.currentMatch = match
+                        setTimeout(() => {this.loading = false}, 1000)
                     }
                 })
             })
--- src/pages/AppInjuries.vue
+++ src/pages/AppInjuries.vue
@@ -3,59 +3,66 @@
         <h1 class="text-center flex items-center justify-center text- text-2xl pt-5">
             Injuries
         </h1>
-        <div class="" v-for="(team, index) in teams" :key="index">
-            <div class="w-full text-center text-lg text-white py-4 px-2 md:mx-3 bg-slate-500">{{ team }}</div>
-            
-            <ul class="grid lg:grid-cols-2 grid-cols-1 gap-8 p-4 text-">           
-                <li  
-                    class="border border-gray-100 shadow-xs md:p-7 p-3 flex items-center justify-between w-full" 
-                    v-for="(player, index) in injuries" 
-                    :key="index" 
-                    v-show="team === player.team"
-                >
-                    <div class="md:flex grid justify-items-center items-center">
-                        <img 
-                            class="h-16 w-16 rounded-full" 
-                            src="../assets/person-icon.jpg" 
-                            alt="PlayerPlaceholder"
-                        >    
-                        <div class="p-2 mx-3">
-                            <h2 class="text-purple-900 md:text-lg py-1">
-                                <strong class="text-black">Player:</strong> 
-                                {{ player.name }} 
-                            </h2>
-                            <h2 class="text-green-600 py-1">
-                                <strong class="text-black">Team:</strong> 
-                                {{ player.team }} 
-                            </h2>
-                        </div>                
-                    </div>
-                    <div class="text-sm">
-                        <p class="italic text-xs py-1 text-red-600">
-                            <strong class="text-black">Injury:</strong> 
-                            {{ player.reason  }}
-                        </p>
-                        <p class="text-xs py-1 text-green-600 font-serif">
-                            <strong class="text-black">Back to board:</strong> 
-                            {{ player.back  }}
-                        </p>
-                    
-                        <div class="flex items-center">
-                            <p class="text-sm text-blue-600 mr-4">{{ player.severity }}/10</p>
-                            <div class="flex md:w-40 w-20 md:h-5 h-3 items-center bg-slate-100 my-5 ">
-                            <div 
-                                class="range md:h-5 h-3 md:w-4 w-2 p-0 m-0" 
-                                :class="player.severity === 10 ? 'bg-green-400' :  player.severity < 5 ? 'bg-red-400' : 'bg-yellow-400'" 
-                                v-for="(range, index) in player.severity" 
-                                :key="index"
-                            >
-                            </div>
-                            </div>                        
+        <div v-if="!loading">
+            <div class="my-5" v-for="(team, index) in teams" :key="index">
+                <div class="w-full text-center text-lg text-white py-4 px-2 md:mx-3 bg-slate-500">
+                    {{ team }}
+                </div>
+                
+                <ul class="grid lg:grid-cols-2 grid-cols-1 gap-8 p-4 text-">           
+                    <li  
+                        class="border border-gray-100 shadow-xs md:p-7 p-3 flex items-center justify-between w-full" 
+                        v-for="(player, index) in injuries" 
+                        :key="index" 
+                        v-show="team === player.team"
+                    >
+                        <div class="md:flex grid justify-items-center items-center">
+                            <img 
+                                class="h-16 w-16 rounded-full" 
+                                src="../assets/person-icon.jpg" 
+                                alt="PlayerPlaceholder"
+                            >    
+                            <div class="p-2 mx-3">
+                                <h2 class="text-purple-900 md:text-lg py-1">
+                                    <strong class="text-black">Player:</strong> 
+                                    {{ player.name }} 
+                                </h2>
+                                <h2 class="text-green-600 py-1">
+                                    <strong class="text-black">Team:</strong> 
+                                    {{ player.team }} 
+                                </h2>
+                            </div>                
                         </div>
-                    </div>
-                </li>
-            </ul>            
-        </div>         
+                        <div class="text-sm">
+                            <p class="italic text-xs py-1 text-red-600">
+                                <strong class="text-black">Injury:</strong> 
+                                {{ player.reason  }}
+                            </p>
+                            <p class="text-xs py-1 text-green-600 font-serif">
+                                <strong class="text-black">Back to board:</strong> 
+                                {{ player.back  }}
+                            </p>
+                        
+                            <div class="flex items-center">
+                                <p class="text-sm text-blue-600 mr-4">{{ player.severity }}/10</p>
+                                <div class="flex md:w-40 w-20 md:h-5 h-3 items-center bg-slate-100 my-5 ">
+                                <div 
+                                    class="range md:h-5 h-3 md:w-4 w-2 p-0 m-0" 
+                                    :class="player.severity === 10 ? 'bg-green-400' :  player.severity < 5 ? 'bg-red-400' : 'bg-yellow-400'" 
+                                    v-for="(range, index) in player.severity" 
+                                    :key="index"
+                                >
+                                </div>
+                                </div>                        
+                            </div>
+                        </div>
+                    </li>
+                </ul>            
+            </div>  
+        </div>
+        <div class="flex items-center justify-center text-4xl  w-full h-screen" v-else>
+            Wait⚽⚽⚽
+        </div>               
     </div>
 </template>
 
@@ -67,13 +74,15 @@ export default {
     mixins: [ BASE_URL ],
     data: () => ({
         injuries: [],
-        teams: []
+        teams: [],
+        loading: false
     }),
     computed: {
 
     },
     methods: {
         getRatings () {
+            this.loading = true
             BASE_URL.get('injuries/').then(({ data }) => {
                 let injury = data.players
                 let teams = []
@@ -82,7 +91,7 @@ export default {
                     teams.push(item.team)
                 })
                 this.teams = teams.filter((item, index) => teams.indexOf(item) === index)
-
+                setTimeout(() => {this.loading = false}, 1000)
             })
             .catch(err => console.log(err))
         }
